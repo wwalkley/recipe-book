ac3b51e301420b4bd3b842b275b676fb2ff4a86d
wwalkley <wwalkley@bravurasolutions.com>
2019-11-12 13:10:42 +1300

    Started refactoring form to use TD approach

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,6 +1,7 @@
 import { ShoppingListService } from "./../shopping-list.service";
 import { Ingredient } from "./../../shared/ingredient.model";
 import { Component, OnInit, ElementRef, ViewChild } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
   selector: "app-shopping-edit",
@@ -8,17 +9,13 @@ import { Component, OnInit, ElementRef, ViewChild } from "@angular/core";
   styleUrls: ["./shopping-edit.component.css"]
 })
 export class ShoppingEditComponent implements OnInit {
-  @ViewChild("nameInput", { static: false }) nameInputRef: ElementRef;
-  @ViewChild("amountInput", { static: false }) amountInputRef: ElementRef;
-
   constructor(private slService: ShoppingListService) {}
 
   ngOnInit() {}
 
-  onAddItem() {
-    const name = this.nameInputRef.nativeElement.value;
-    const amount: number = Math.floor(this.amountInputRef.nativeElement.value);
-    const newIngredient = new Ingredient(name, amount);
+  onAddItem(form: NgForm) {
+    const value = form.value;
+    const newIngredient = new Ingredient(value.name, value.amount);
     this.slService.addItem(newIngredient);
   }
 }
